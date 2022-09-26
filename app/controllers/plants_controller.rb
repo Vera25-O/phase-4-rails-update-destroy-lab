f7bd9a99dd513ff6a3443eca7f34f20e44d03174
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-09-26 16:42:01 +0300

    updated controller file

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,22 +1,41 @@
 class PlantsController < ApplicationController
-
   # GET /plants
   def index
     plants = Plant.all
     render json: plants
   end
-
   # GET /plants/:id
   def show
     plant = Plant.find_by(id: params[:id])
     render json: plant
   end
-
   # POST /plants
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
+# PATCH /birds/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant
+    else
+      render json: {error: "Plant not found"}, status: :not_found
+    end
+  end
+
+  #DELETE /plants
+
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      render json: []
+      else
+        render json: {error: "Plant not found"}, status: :not_found
+      end
+    end
 
   private
 
